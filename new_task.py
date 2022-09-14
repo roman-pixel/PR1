779e4d515aa8d0d51ff6fc69a40dfbecadbfe84c
sadovrs <sadovrs@ARMCSR17>
2022-09-14 11:01:59 +0300

    example 2

---
+++ new_task.py
@@ -0,0 +1,13 @@
+from email import message
+import sys
+import pika
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='hello')
+message = ' '.join(sys.argv[1:]) or "Hello World!"
+channel.basic_publish(exchange='', routing_key='task_queue',
+                      body=message, properties=pika.BasicProperties(delivery_mode=2))
+print("[x] Sent %r" % message)
+connection.close()
